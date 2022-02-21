4c16d4692922304406b4d3044d0ae73e6531fd8e
mosePark <58103517+mosePark@users.noreply.github.com>
2022-02-21 21:34:05 +0900

    Update 1932: 정수 삼각형.py
    Solution of BOJ 1932

--- 동적 계획법 1/1932: 정수 삼각형.py
+++ 동적 계획법 1/1932: 정수 삼각형.py
@@ -12,3 +12,23 @@ for i in range(1, n) :
     ans += max(arr[i-1][], arr[i-1][]) + arr[i][]
 
 print(ans)
+
+# 정답 코드
+def sol(arr) :
+    for i in range(1, len(arr)) :
+        for j in range(i+1) :
+            if j == 0 :
+                arr[i][j] += arr[i-1][j]
+            elif j == i :
+                arr[i][j] += arr[i-1][j-1]
+            else :
+                arr[i][j] += max(arr[i-1][j-1], arr[i-1][j])
+    return max(arr[-1])
+
+n = int(input())
+arr = []
+for _ in range(n) :
+    nums = list(map(int,input().split()))
+    arr.append(nums)
+
+print(sol(arr))
