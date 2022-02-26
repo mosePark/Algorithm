6eab875950979244d67948878f905d60da925b68
mosePark <58103517+mosePark@users.noreply.github.com>
2022-02-26 21:10:09 +0900

    Create 1026: 보물.py
    Solution of BOJ 1026

---
+++ 수학/1026: 보물.py
@@ -0,0 +1,15 @@
+# 1026: 보물
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a = sorted(a)
+b = sorted(b, reverse=True)
+
+ans = 0
+
+for i in range(n) :
+    ans += a[i] * b[i]
+
+print(ans)
